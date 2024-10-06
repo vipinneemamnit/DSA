4e56280c36492bba0701c41fe9223195fa0e21ce
vipinneemamnnit <vipinneemamnnit@gmail.com>
2024-10-07 04:57:51 +0530

    LL reversal

---
+++ Leetcode/reverseLinkedList.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int new_data) {
+        data = new_data;
+        next = nullptr;
+    }
+};
+    //Node(int value): data(value),next(nullptr){}
+
+
+Node* reverseList(Node* head){
+    Node* curr = head, *prev = nullptr, *next;
+
+    while(curr){
+        next = curr->next;
+        curr->next = prev;
+        prev= curr;
+        curr = next;
+        
+    }
+    return prev;
+}
+
+void printList(Node* node) {
+    while (node != nullptr) {
+        cout << " " << node->data;
+        node = node->next;
+    }
+}
+int main() {
+
+    // Create a hard-coded linked list:
+    // 1 -> 2 -> 3 -> 4 -> 5
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Given Linked list:";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "\nReversed Linked List:";
+    printList(head);
+
+    return 0;
+}
